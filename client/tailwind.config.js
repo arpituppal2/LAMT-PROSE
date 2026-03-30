b1a31f07513f45c019d23c512ff71d2c22112805
Arpit Uppal <someone040807@gmail.com>
2026-03-30 00:05:32 -0700

    style: add full UCLA color palette to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class', // <-- Add this line right here!
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -8,11 +8,19 @@ export default {
   theme: {
     extend: {
       colors: {
+        // UCLA Primary
         'ucla-blue': '#2774AE',
         'ucla-gold': '#FFD100',
+        // UCLA Blue Tones
+        'ucla-darkest-blue': '#003B5C',
+        'ucla-darker-blue': '#005587',
         'ucla-dark-blue': '#005587',
-        'ucla-light-blue': '#8BB8E8',
-      }
+        'ucla-lighter-blue': '#8BB8E8',
+        'ucla-lightest-blue': '#DAEBFE',
+        // UCLA Gold Tones
+        'ucla-darkest-gold': '#FFB81C',
+        'ucla-darker-gold': '#FFC72C',
+      },
     },
   },
   plugins: [],

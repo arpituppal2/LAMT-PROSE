ca26f5cb855d08c029be9d2d991d45724914556f
Arpit Uppal <someone040807@gmail.com>
2026-04-17 12:44:17 -0700

    Add /notifications route to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import ExamManager from './pages/ExamManager';
 import ExamDetail from './pages/ExamDetail';
 import ArchivePage from './pages/ArchivePage';
 import TransferGuest from './pages/TransferGuest';
+import Notifications from './pages/Notifications';
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
@@ -77,6 +78,7 @@ function App() {
           <Route path="/users/:id" element={<PrivateRoute><UserProfile /></PrivateRoute>} />
           <Route path="/profile" element={<PrivateRoute><UserProfile /></PrivateRoute>} />
           <Route path="/archive" element={<PrivateRoute><ArchivePage /></PrivateRoute>} />
+          <Route path="/notifications" element={<PrivateRoute><Notifications /></PrivateRoute>} />
 
           {/* Admin Routes */}
           <Route path="/admin/transfer" element={<AdminRoute><TransferGuest /></AdminRoute>} />
